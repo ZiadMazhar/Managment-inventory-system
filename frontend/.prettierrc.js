1a38113a5f896602785e5500bec75f236dda8e59
Ziad <Ziad255599@gmail.com>
2023-05-09 02:34:41 +0300

    add front end

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "editor.formatOnSave": false
+
+    // semi: false,
+    // trailingComma: "all",
+    // singleQuote: true,
+    // printWidth: 100,
+    // tabWidth: 2
+  };
